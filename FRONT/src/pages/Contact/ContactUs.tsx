d39d7a088e1d12ba209d00488cb6bc95b0c3c6f1
Agence universelle <universelle.ma@gmail.com>
2025-12-25 14:56:30 +0100

    Update ContactUs.tsx

--- FRONT/src/pages/Contact/ContactUs.tsx
+++ FRONT/src/pages/Contact/ContactUs.tsx
@@ -37,7 +37,7 @@ const ContactUs = () => {
     }
 
     try {
-      const res = await fetch(`${API_BASE_URL}/contact`, {
+const res = await fetch(`${API_BASE_URL}/api/contact`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
