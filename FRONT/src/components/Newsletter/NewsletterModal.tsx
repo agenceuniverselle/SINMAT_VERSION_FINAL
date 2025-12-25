1e390301a592e1dcfbd2d4781fe7ffad1f57d910
Agence universelle <universelle.ma@gmail.com>
2025-12-25 15:15:39 +0100

    Update NewsletterModal.tsx

--- FRONT/src/components/Newsletter/NewsletterModal.tsx
+++ FRONT/src/components/Newsletter/NewsletterModal.tsx
@@ -41,7 +41,7 @@ const NewsletterModal = ({ open, onOpenChange }: NewsletterModalProps) => {
     setLoading(true);
 
     try {
-      const res = await fetch(`${API_BASE_URL}/newsletter`, {
+      const res = await fetch(`${API_BASE_URL}/api/newsletter`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
