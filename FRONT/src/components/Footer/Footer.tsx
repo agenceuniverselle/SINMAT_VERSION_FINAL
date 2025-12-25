2f6601483c06074a4298622675c9874840a19306
Agence universelle <universelle.ma@gmail.com>
2025-12-25 15:16:56 +0100

    Update Footer.tsx

--- FRONT/src/components/Footer/Footer.tsx
+++ FRONT/src/components/Footer/Footer.tsx
@@ -24,7 +24,7 @@ const Footer = () => {
 
     setSending(true);
     try {
-      const res = await fetch(`${API_BASE_URL}/newsletter`, {
+      const res = await fetch(`${API_BASE_URL}/api/newsletter`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
